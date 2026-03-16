aa7b83286597ed88f0af5c08b37ce3cc39a79d62
Galaceon <antoniogarcialtr@gmail.com>
2026-03-16 06:08:08 +0100

    creating a type for itemID

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -42,11 +42,11 @@ export const useCart = () => {
         }
     }
 
-    function removeFromCart(id) {
+    function removeFromCart(id : Guitar['id']) {
         setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
     }
 
-    function increaseQuantity(id) {
+    function increaseQuantity(id : Guitar['id']) {
         const updatedCart = cart.map( item => {
             if(item.id === id && item.quantity < MAX_ITEMS) { // Al ser el item buscado se actualiza para updatedCart
                 return {
@@ -59,7 +59,7 @@ export const useCart = () => {
         setCart(updatedCart)
     }
 
-    function decreaseQuantity(id) {
+    function decreaseQuantity(id : Guitar['id']) {
         const updatedCart = cart.map( item => {
             if(item.id === id && item.quantity > MIN_ITEMS) {
                 return {
